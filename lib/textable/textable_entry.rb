c30c4ddaed3253a1f7f01e79238cdcc708b726d7
Justin Perkins <justinperkins@gmail.com>
2014-10-10 11:32:42 -0500

    forgot to add the attr accessibles

--- lib/textable/textable_entry.rb
+++ lib/textable/textable_entry.rb
@@ -1,6 +1,8 @@
 class TextableEntry < ActiveRecord::Base
   belongs_to  :item, :polymorphic => true
 
+  attr_accessible :item_id, :item_type, :item_fieldname
+
   # Find or create the textable row
   def self.find_or_create_for(name, obj, options)
     self.find_or_create_by_item_id_and_item_type_and_item_fieldname(obj.id, obj.class.table_name.classify, name.to_s)
